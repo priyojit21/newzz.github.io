a4efd3eb46b553b3d54e597b0f4146083c62f416
priyojit21 <102255292+priyojit21@users.noreply.github.com>
2023-06-20 21:30:48 +0530

    a news app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const https = require("https");
+const app = new express();
+const path = require("path");
+const cors = require("cors");
+
+app.use(cors({
+    origin: "http://localhost:3000",
+}))
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname ,"public", "news.html"));
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server running");
+});
